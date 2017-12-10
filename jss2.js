d8b6cf5304f44b36e6ae741acf83723604bedd53
allindiahacker <allindiahacker@gmail.com>
2017-12-10 06:30:35 +0530

    Update jss2.js

--- jss2.js
+++ jss2.js
@@ -5,9 +5,10 @@ var click_time = start_time;
 var timer_duration;
 var elem_opacity = 0;
 var count =0;
+var timer_to_start=true;
+var page_fade_in_state = 1;
 var setInterval_id = setInterval(function(){ fade_in("game") },30);
-setTimeout(page_explain,660);
-setTimeout(button_appear,1200);
+var setInterval_id_timer = setInterval_id;
 window.onclick = counter;
 function counter()
 {
@@ -47,108 +48,92 @@ function counter()
 function timer_decreaser()
 {
     if(time_decrease>=0)
-        {
-            document.getElementById("timer_image2").style.width = time_decrease + 'px';
-            time_decrease-=(0.175)*(60/(timer_duration/1000));
-            setTimeout(timer_decreaser,25);
-        }
+    {
+        document.getElementById("timer_image2").style.width = time_decrease + 'px';
+        time_decrease-=(0.175)*(60/(timer_duration/1000));    
+    }
+    else
+    {
+        clearInterval(setInterval_id_timer);
+        click_time = 0;
+        elem_opacity = 1;
+        page_fade_in_state = 4;
+        setInterval_id = setInterval(function(){ fade_out2("timer_page") },25);
+        document.getElementById("count3").innerHTML = "Wow, you literally spent all that time just clicking randomly on the screen. Seriously, pls get a life, and like you managed " + count + " clicks, which doesn't really matter"; 
+        document.getElementById("count3").style.fontFamily = "raleway";
+        document.getElementById("count3").style.fontSize = "60px";
+        document.getElementById("count3").style.fontWeight = "200";
+        document.getElementById("count3").style.width = "150px;";
+        document.getElementById("count3").style.display = 'block';
+        
+        
+    }
     
 }
 function fade_in(elem)
 {
-    if(elem_opacity<=1)
+    if(elem_opacity<1)
     {
         var object = document.getElementById(elem);
-        object.style.opacity=elem_opacity;
         elem_opacity+=0.05;
+        object.style.opacity=elem_opacity;
+        
     }
     else
     {
         clearInterval(setInterval_id);  
-        
+        if(page_fade_in_state==1)
+        {   
+            page_fade_in_state=2;
+            elem_opacity = 0;
+            setInterval_id = setInterval(function(){ fade_in("explain") },25);
+        }
+        else if(page_fade_in_state==2)
+        {
+            page_fade_in_state=3;
+            elem_opacity = 0;
+            setInterval_id = setInterval(function(){ fade_in("start") },25);
+        }
     }
       
 }
 
-function page_explain()
-{   
-    elem_opacity = 0;
-    setInterval_id = setInterval(function(){ fade_in("explain") },25);
-}
-
-function button_appear()
-{   
-    elem_opacity = 0;
-    setInterval_id = setInterval(function(){ fade_in("start") },25);
+function fade_out2(elem)
+{
+    if(elem_opacity>0)
+    {
+        var object = document.getElementById(elem);
+        elem_opacity-=0.05;
+        object.style.opacity=elem_opacity;
+        
+    }
+    else{
+        clearInterval(setInterval_id);
+        if(timer_to_start==true)
+        {   timer_to_start = false;
+            document.getElementById("all_page1").style.display='none';
+            document.getElementById("timer_image").style.display='block';
+            elem_opacity = 0;
+            setInterval_id = setInterval(function(){ fade_in("timer_page") },25);
+            click_time = new Date();
+            setInterval_id_timer = setInterval(function(){ timer_decreaser() },25);
+            
+        }
+        if(page_fade_in_state==4)
+        {
+            elem_opacity = 0;
+            setInterval_id_timer = setInterval(function(){ fade_in("count3") },50);
+            document.getElementById("timer_page").style.display = 'none';
+        }
+    }
 }
 function fade_out_toggle()
 {
     elem_opacity = 1;
-    setInterval_id = setInterval(function(){ fade_out() },25);
-    setTimeout(hide_all,520);
+    setInterval_id = setInterval(function(){ fade_out2("all_page1") },25);
     var form = document.getElementById("frm1");
     timer_duration = form.elements[0].value;
     timer_duration*=1000;
     return false;
 }
-function fade_out()
-{
-    if(elem_opacity>-0.05)
-    {
-        var object = document.getElementById("all");
-        object.style.opacity=elem_opacity;
-        elem_opacity-=0.05;
-    }
-    else{
-        clearInterval(setInterval_id);
-    }
-}
-function hide_all()
-{
-    var object = document.getElementById("all");
-    object.style.display = "none";
-    elem_opacity = 0;
-    bar_display();
-    setTimeout(timer_end,timer_duration+1000);
-    setTimeout(score_display,timer_duration+1750);
-}
-function score_display()
-{
-    document.getElementById("count3").innerHTML = "Wow, you literally spent all that time just clicking randomly on the screen. Seriously, pls get a life, and like you managed " + count + " clicks, which doesn't really matter"; 
-    document.getElementById("count3").style.fontFamily = "raleway";
-    document.getElementById("count3").style.fontSize = "60px";
-    document.getElementById("count3").style.fontWeight = "200";
-    document.getElementById("count3").style.width = "150px;";
-    document.getElementById("count3").style.display = 'block';
-    document.getElementById("count").style.display = 'none';
-    click_time = 0;
-    setInterval_id = setInterval(function(){ fade_in("count3") },25);
-    
-}
-function timer_end()
-{
-    if(elem_opacity>=-0.05)
-    {
-        document.getElementById("back_bar").style.opacity=elem_opacity;
-        document.getElementById("count").style.opacity=elem_opacity;
-        document.getElementById("timer_image").style.opacity=elem_opacity;
-        elem_opacity-=0.05;
-        setTimeout(timer_end,25);
-    }
-}
-function bar_display()
-{    document.getElementById("timer_image").style.display = 'block';
-    if(elem_opacity<=1.05)
-    {   
-        document.getElementById("back_bar").style.opacity=elem_opacity;
-        document.getElementById("count").style.opacity=elem_opacity;
-        document.getElementById("count2").style.opacity=elem_opacity;
-        document.getElementById("timer_image").style.opacity=elem_opacity;
-        elem_opacity+=0.05;
-        setTimeout(bar_display,25);
-    }
-    else{
-        click_time = new Date();
-        timer_decreaser();
-    }
-}
+
